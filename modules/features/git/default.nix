da5d0a3609fa9ad3573458260afe060e6c0c2137
Harshil Bhatt <harshilbhatt2001@gmail.com>
2026-08-19 14:32:14 +0200

    feat(git): gs and gg fish abbreviations

--- modules/features/git/default.nix
+++ modules/features/git/default.nix
@@ -1,5 +1,10 @@
 { ... }: {
   flake.nixosModules.git = {
     programs.git.enable = true;
+
+    programs.fish.shellAbbrs = {
+      gs = "git status";
+      gg = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an (%ae)%C(reset)' --all";
+    };
   };
 }
